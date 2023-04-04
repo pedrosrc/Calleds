e31e608d4716f9f12eecb9ff3061382c276edfd0
Pedro Leonardo <pedroleon.pc@hotmail.com>
2023-04-04 17:46:01 -0300

    update

--- calleds-app/src/pages/Profile/Profile.tsx
+++ calleds-app/src/pages/Profile/Profile.tsx
@@ -1,13 +1,50 @@
+import { useContext, useState } from "react"
 import NavBar from "../../components/Nav/NavBar"
 import Title from "../../components/Title/Title"
+import avatar from '../../assets/profile-user.png'
+import { AuthContext } from "../../contexts/auth"
+import { FiUpload } from "react-icons/fi"
+
 import './profile.css'
-export default function Profile(){
-    return(
+export default function Profile() {
+
+    const { user }: any = useContext(AuthContext);
+
+    const [avatarUrl, setAvatarUrl] = useState<any>(user && user.avatarUrl)
+
+    return (
         <div className="container_profile">
-            <NavBar/>
+            <NavBar />
 
             <div className="content">
-                <Title title="Configurações"/>
+                <Title title="Configurações" />
+                <div className="container_user">
+                    <form className="form_profile">
+                        <h1>Edite Seu Perfil:</h1>
+                        <label className="avatar_label">
+                            <span><FiUpload color="#fff" size={25} /></span>
+                            <input type="file" accept="image/*" /> <br />
+                            {avatarUrl === null ? (
+                                <img src={avatar} alt="foto de perfil" width={250} />
+                            ) : (
+                                <img src={avatarUrl} alt="foto de perfil" width={250} />
+                            )}
+                            </label>
+                            <div className="info_user">
+                            <label>
+                                Nome
+                            </label>
+                            <input type="text" placeholder="Seu nome" />
+                            <label>
+                                Email
+                            </label>
+                            <input type="text" placeholder="Seu email" disabled={true}/>
+
+                            <button type="submit">Salvar</button>
+                        </div>
+                        
+                    </form>
+                </div>
             </div>
         </div>
     )
